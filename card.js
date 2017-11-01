2c625c13e137b168d622a218ff8b123df6651832
jwmunn <contact@justinmunn.co>
2017-10-31 18:53:40 -0700

    Build a deck with arrow functions.

--- card.js
+++ card.js
@@ -8,6 +8,22 @@ class Card {
     console.log(`${this.rank} of ${this.suit}`);
   }
 }
-const card = new Card(9, "diamonds");
 
-card.outputCard();
+class Deck {
+  constructor() {
+    const ranks = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+    const suits = ["spades", "clubs", "hearts", "diamonds"];
+    this.cards = [];
+
+    ranks.forEach( (rank) => {
+      suits.forEach( (suit) => {
+        const card = new Card(rank, suit);
+        this.cards.push(card);
+      });
+    });
+  }
+}
+
+const deck = new Deck();
+
+console.log(deck);
